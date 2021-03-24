079bf07c61aaf8ffb7a38b7631b1eb65703fa3b8
Rumman Ashraf <rumman.ashraf@gmail.com>
2021-03-24 12:04:59 +0600

    Added Basic Structure for Burger Builder

---
+++ src/components/Layouts/Layouts.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Aux from '../../hoc/aux'
+
+import classes from './Layout.module.css'
+
+const layout = (props) => (
+    <Aux>
+        <div>Toolbars, Sidebar, BackDrop</div>
+        <main className={classes.Content}>
+            {props.children}
+        </main>
+    </Aux>
+);
+
+export default layout;
