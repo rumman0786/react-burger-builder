2d44d94d10bb24074f729f606c0035e2eb348a6a
Rumman Ashraf <rumman.ashraf@gmail.com>
2021-04-07 08:26:11 +0600

    Optimized Component Rendering with life cycle method in Modal and Order Summary

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,28 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import Aux from '../../../hoc/aux'
 import Button from '../../UI/Button/Button'
 
-function orderSummary(props) {
-    const ingridientSummary = Object.keys(props.ingridients)
+class OrderSummary extends Component {
+
+    componentDidUpdate() {
+        console.log("[Order Summary Update Triggered]");
+    }
+
+    render() {
+        const ingridientSummary = Object.keys(this.props.ingridients)
                               .map(igKey => {
-                                    return <li key={igKey}><span className={{textTransformat: 'capitalize'}}>
-                                            {igKey}
-                                        </span>: {props.ingridients[igKey]}</li>
+                                    return <li key={igKey}>
+                                                <span className={{textTransformat: 'capitalize'}}>{igKey}</span>
+                                                : {this.props.ingridients[igKey]}
+                                            </li>
                               });
-    return (
-        <Aux>
-            <h3>Your Order</h3>
-            <p>A Delicious Burger is Prepeared with following ingridiens:</p>
-            <ul>
-                {ingridientSummary}
-            </ul>
-            <p>Total Price: <strong>{props.price.toFixed(2)}</strong></p>
-            <p>Continue to Checkout</p>
-            <Button buttonType='Danger' clicked={props.cancelled}>Cancel</Button>
-            <Button buttonType='Success' clicked={props.continued}>Continue</Button>
-        </Aux>
-    );
+                              
+        return(
+            <Aux>
+                <h3>Your Order</h3>
+                <p>A Delicious Burger is Prepeared with following ingridiens:</p>
+                <ul>
+                    {ingridientSummary}
+                </ul>
+                <p>Total Price: <strong>{this.props.price.toFixed(2)}</strong></p>
+                <p>Continue to Checkout</p>
+                <Button buttonType='Danger' clicked={this.props.cancelled}>Cancel</Button>
+                <Button buttonType='Success' clicked={this.props.continued}>Continue</Button>
+            </Aux>
+        );
+    }
 }
 
-export default orderSummary;
+export default OrderSummary;
--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,26 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import styles from './Modal.module.css';
 import Aux from '../../../hoc/aux'
 import Backdrop from '../Backdrop/Backdrop'
 
-const modal = (props) => {
-    return (
-        <Aux>
-            <Backdrop
-                show={props.showSummary}
-                closed={props.backdropHandler}/>
+class Modal extends Component {
 
-            <div className={styles.Modal}
-                style={
-                    { transform: props.showSummary ? 'translateY(0)' : 'translateY(-100vh)',
-                    opacity: props.showSummary ? '1' : '0'
-                    }
-                }>
-                {props.children}
-            </div>
-        </Aux>
-    );
+    shouldComponentUpdate(nextProps, nextState) {
+        return nextProps.showSummary !== this.props.showSummary;
+    }
+
+    componentDidUpdate() {
+        console.log("[Modal] component Updated");
+    }
+
+    render() {
+        return (
+            <Aux>
+                <Backdrop
+                    show={this.props.showSummary}
+                    closed={this.props.backdropHandler}/>
+    
+                <div className={styles.Modal}
+                    style={
+                        { transform: this.props.showSummary ? 'translateY(0)' : 'translateY(-100vh)',
+                        opacity: this.props.showSummary ? '1' : '0'
+                        }
+                    }>
+                    {this.props.children}
+                </div>
+            </Aux>
+        );
+    }
+    
 }
 
-export default modal;
+export default Modal;
