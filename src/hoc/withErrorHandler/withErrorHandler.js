e4ad87547e6f8439be7070c1ec0e1cb15e024d9b
Rumman Ashraf <rumman.ashraf@gmail.com>
2021-04-11 12:44:07 +0600

    Removed interceptors after use

--- src/hoc/withErrorHandler/withErrorHandler.js
+++ src/hoc/withErrorHandler/withErrorHandler.js
@@ -13,14 +13,19 @@ const withErrorHandler = (WrappedComponent, axios) => {
                 error: null
             };
 
-            axios.interceptors.request.use(request => {
+            this.requestInterceptor = axios.interceptors.request.use(request => {
                 this.setState({error: null});
                 return request;
             });
 
-            axios.interceptors.response.use(null, error => {
+            this.responseInterceptor = axios.interceptors.response.use(null, error => {
                 this.setState({error: error});
             });
+        }
+
+        componentWillUnmount() {
+            axios.interceptors.request.eject(this.requestInterceptor);
+            axios.interceptors.response.eject(this.responseInterceptor);
         }
 
         errorHandler = () => {
