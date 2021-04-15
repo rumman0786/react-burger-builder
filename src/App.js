e78852af837f195974dde311f8487a0fe63a1c51
Rumman Ashraf <rumman.ashraf@gmail.com>
2021-04-15 11:08:23 +0600

    Added Checkout and Summary

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react';
 import Layout from './hoc/Layouts/Layouts';
 
 import BurgerBuilder from '../src/containers/BurgerBuilder/BurgerBuilder'
+import Checkout from '../src/containers/Checkout/Checkout'
 
 class App extends Component {
   render() {
     return (
       <Layout>
         <BurgerBuilder/>
+        <Checkout/>
       </Layout>
     );
   }
---
+++ src/components/Order/CheckoutSummary/CheckoutSummary.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import Burger from '../../Burger/Burger';
+import Button from '../../../components/UI/Button/Button';
+
+import styles from './CheckoutSummary.module.css';
+
+const checkoutSummary = (props) => (
+    <div className={styles.CheckoutSummary}>
+        <h1>We hope you enjoy the Burger!</h1>
+        <div style={{width:'100%', margin:'auto'}}>
+            <Burger ingridients={props.ingridients}/>
+        </div>
+        <Button buttonType="Danger">Cancel</Button>
+        <Button buttonType="Success">Continue</Button>
+    </div>        
+);
+
+export default checkoutSummary;
