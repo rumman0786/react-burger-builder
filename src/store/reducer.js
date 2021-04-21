6d4e7e6e4157c7e782a41f6865ce9cfaf38ba8bd
Rumman Ashraf <rumman.ashraf@gmail.com>
2021-04-21 21:47:32 +0600

    Added ingridient actions

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,7 +1,12 @@
 import * as actionType from './actions';
 
 const initialState = {
-    ingridients: null,
+    ingridients: {
+        salad: 0,
+        bacon: 0,
+        cheese: 0,
+        meat: 0
+    },
     totalPrice: 4.0
 }
 
@@ -9,18 +14,26 @@ const reducer = (state=initialState, action) => {
     switch(action.type) {
         case actionType.ADD_INGRIDIENT:
             return {
-
+                ...state,
+                ingridients : {
+                    ...state.ingridients,
+                    [action.ingridientName]: state.ingridients[action.ingridientName] + 1
+                }
             };
 
         case actionType.REMOVE_INGRIDIENT:
             return {
-
+                ...state,
+                ingridients : {
+                    ...state.ingridients,
+                    [action.ingridientName]: state.ingridients[action.ingridientName] - 1
+                }
             };
 
         default:
             return state;
     }
-    
+
     return state;
 }
 
